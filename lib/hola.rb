27319e56d0b04d9f2e2d69f6634645dc60ec24a6
Angel buzany <abuzzany@gmail.com>
2017-12-11 22:29:39 -0600

    First commit

---
+++ lib/hola.rb
@@ -0,0 +1,18 @@
+# The main Hola driver
+class Hola
+  # Say hi to the world!
+  #
+  # Example:
+  #   >> Hola.hi("spanish")
+  #   => hola mundo
+  #
+  # Arguments:
+  #   language: (String)
+
+  def self.hi(language = "english")
+    translator = Translator.new(language)
+    puts translator.hi
+  end
+end
+
+require 'hola/translator'
